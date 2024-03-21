56a80c3c3ecd03eb2729a5830732dad0a86931f7
snow-fox <taijunjet@hotmail.com>
2024-03-21 18:18:35 +0000

    comments

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
-set -l current_file (status filename)  # Gets the name of the current script
+# gets the name of the current script
+set -l current_file (status filename)
 
+# source all *.fish files except this one
 for file in ~/.config/fish/*.fish
   if test "$file" != "$current_file"
     source $file
