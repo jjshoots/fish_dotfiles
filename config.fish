66852a660957d6dbee2fc7932ec3b44a38aa5ae7
snow-fox <taijunjet@hotmail.com>
2024-03-21 18:18:11 +0000

    formatting

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set -l current_file (status filename)  # Gets the name of the current script
 
 for file in ~/.config/fish/*.fish
-    if test "$file" != "$current_file"
-        source $file
-    end
+  if test "$file" != "$current_file"
+    source $file
+  end
 end
