9d63398132177146190d72c3da2167ff697cf5f1
Jet <taijunjet@hotmail.com>
2024-06-03 23:40:02 +0900

    update install

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# try to install fish if it doesn't already exist
+if ! command -v fish &> /dev/null
+then
+    echo "fish not found, installing latest from apt"
+    if ! grep -r "fish-shell/release-3" /etc/apt/sources.list /etc/apt/sources.list.d/; then
+        sudo apt-add-repository -y ppa:fish-shell/release-3
+    else
+        echo "Repository fish-shell/release-3 is already added."
+    fi
+    sudo apt update && sudo apt upgrade
+    sudo apt install fish
+    sudo chsh -s /usr/bin/fish
+fi
+
 sudo apt install fzf
 sudo apt install trash-cli
 sudo apt install exa
