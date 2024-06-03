b83773436ba8ec75b93a1872dd5390b3c7299855
Jet <taijunjet@hotmail.com>
2024-06-04 00:01:02 +0900

    grammar

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # try to install fish if it doesn't already exist
 if ! command -v fish &> /dev/null
 then
-    echo "fish not found, installing latest from apt"
+    echo "fish not found, installing latest from apt..."
     if ! grep -r "fish-shell/release-3" /etc/apt/sources.list /etc/apt/sources.list.d/; then
         sudo apt-add-repository -y ppa:fish-shell/release-3
     else
-        echo "Repository fish-shell/release-3 is already added."
+        echo "Repository fish-shell/release-3 is already added. Skipping..."
     fi
     sudo apt update && sudo apt upgrade
     sudo apt install fish
@@ -17,8 +17,7 @@ fi
 sudo apt install fzf
 sudo apt install trash-cli
 sudo apt install exa
-sudo apt install tmux
 sudo apt install resolvconf
 sudo apt install wireguard
 sudo apt install bat
-ln -s /usr/bin/batcat ~/.local/bin/bat
+sudo ln -s /usr/bin/batcat ~/.local/bin/bat
