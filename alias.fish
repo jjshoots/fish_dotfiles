2d965c75ca367ff175a2af9f7c09bd0a66fe17ea
Jet <taijunjet@hotmail.com>
2024-06-18 11:15:05 +0900

    unalias rm, I think it's muscle memory now

--- alias.fish
+++ alias.fish
@@ -1,5 +1,5 @@
 # STOP USING RM
-alias rm='echo "This is not the command you are looking for."; false'
+# alias rm='echo "This is not the command you are looking for."; false'
 alias tp=trash-put
 
 # change ls to exa
