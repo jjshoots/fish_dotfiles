82c88bff5a08038327d3f60a98ddc5cef998f661
Jun Jet Tai <jet@white-cat.local>
2025-01-23 22:39:14 -0800

    stash mac os install

--- alias.fish
+++ alias.fish
@@ -1,10 +1,5 @@
-# STOP USING RM
-# alias rm='echo "This is not the command you are looking for."; false'
-alias tp=trash-put
+# macOS empty trash
+alias tp "trash"
 
 # change ls to exa
 alias ls=eza\ --icons
-
-# vpn enable
-alias vpnup "sudo wg-quick up wg0"
-alias vpndown "sudo wg-quick down wg0"
