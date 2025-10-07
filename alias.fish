5a0c5acd05e3fe7a42fa5c4fe2686061451a55b2
jjshoots <taijunjet@hotmail.com>
2025-10-07 11:32:03 +0800

    remove = to be consistent

--- alias.fish
+++ alias.fish
@@ -12,5 +12,5 @@ alias vpn-down "sudo wg-quick down wg0"
 alias cap "caffeinate -d python3"
 
 # clean up .DS_Store files
-alias clean_ds="find ~ -name '.DS_Store' -type f -delete"
-alias clean_ds_all="sudo find / -name '.DS_Store' -type f -delete 2>/dev/null"
+alias clean_ds "find ~ -name '.DS_Store' -type f -delete"
+alias clean_ds_all "sudo find / -name '.DS_Store' -type f -delete 2>/dev/null"
